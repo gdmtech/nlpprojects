3658073706015db371e81919b40aff24d3691c9c
Global Digital MOJO : Deeptech Practice <49622254+gdmtech@users.noreply.github.com>
2021-08-07 15:22:41 +0800

    doc parse and pipeline updates

--- nlp_api.py
+++ nlp_api.py
@@ -73,10 +73,74 @@ def mojo_text_summary(text, limit):
     return ' '.join(summary)
 
 
+def mojo_text_parse(text):
+
+    tokens = []
+    words = []
+    sents = []
+    stop_words = []
+    filtered = []
+
+    print("summarise the text: words, sentences, removed stops, lemmas")
+    # preprocess: reduce the text to all lower cases
+    doc = nlp(text.lower())
+    # preprocess: tokens
+    for token in doc:
+        tokens.append(token.text)
+
+    print('Tokens of all kinds', tokens)
+
+    print('Pipleines - what do we have', tokens)
+
+    print('Pipelines', nlp.pipe_names)
+    print('Add sentencizer')
+
+    nlp.add_pipe('sentencizer')
+    print('Pipelines', nlp.pipe_names)
+
+    # list all the sentences in the text
+    for sent in doc.sents:
+        sents.append(sent.text)
+    print(sents)
+
+    print('remove stop words and classify text')
+    for word in doc:
+        # is_stop is a spacy function
+        if word.is_stop == False:
+            filtered.append(word)
+            # word,pos_ classifies the Part Of Speech (POS) of the text, word.lemma_ lists the related workds
+            print(word.text, word.pos_)
+            print(word.text, word.lemma_)
+    print("Filtered Sentence:", filtered)
+
+    print('entity analysis')
+    # create a 3 part tuple using a for loop of attricutes of each entiy in the text
+    entities = [(i, i.label_, i.label) for i in doc.ents]
+    print('Entities', entities)
+    # use the spacy renderer to serve a web page of the ENTITIES in the text
+    spacy.displacy.serve(doc, style="ent")
+
+
+def spacystopwords(limit):
+    spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+
+    # Printing the total number of stop words:
+    print('Number of stop words:', len(spacy_stopwords))
+
+    # Printing first limit stop words:
+    print('First stop words:', list(spacy_stopwords)[:limit])
+
+
 # open the txt file for analysis
 print('COGNIAI NLP DEMO')
-print('Document Summary')
-text = open("samples/crime.txt", "r")
-print(mojo_text_summary(text.read(), 10))
-
-print('Document Classification : rommance, drama, comedy, etc')
+print('0. List Spacy Stop Words')
+spacystopwords(30)
+print('1. Text Summary')
+text_file = open("samples/crime.txt", "r")
+text = text_file.read()
+print(text)
+print(mojo_text_summary(text, 10))
+print('2. Text Parsing : words, lemmas, entities')
+text = 'How about running a trip to Tokyo?  Dont be shy.  Or perhaps Kyoto or London. Nevertheless, challenges await you if you run it.  An the Financial Times will be interested.  Soon!'
+print(text)
+print(mojo_text_parse(text))
