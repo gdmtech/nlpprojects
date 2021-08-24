c2180015c1f08a0072793a9835d9d45bf6eaece9
Global Digital MOJO : Deeptech Practice <49622254+gdmtech@users.noreply.github.com>
2021-08-24 17:36:12 +0800

    Topic Wordcloud Update

--- nlp_api_topics.py
+++ nlp_api_topics.py
@@ -10,9 +10,8 @@
 # Depedencies
 # ml_datasets
 
-import pyLDAvis.gensim
 import pprint
-from wordcloud import WordCloud
+
 from ml_datasets import imdb
 import spacy
 # import spefiic spacy components that we will use in a pipeline
@@ -57,6 +56,11 @@ from gensim.test.utils import datapath
 
 import string
 
+# for visualisating npl results
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
 
 # endpoint TEXT SUMMARY
 # summarise using sentences with high frequency words (normalised)
@@ -76,11 +80,25 @@ def topic_model2():
     # SETUP A NEW STOP WORD LIST THAT IS SPECIFIC TO THE TARGET TEXT
     # My list of stop words.
     # we will use one document
-    input = "I wish that I am somewhere else with Mrs. Smith.  I want to fly to new york or tokyo.  Anywhere but here,say.  I'll book a ticket tomorrow.  Do I have any money?  I will need money when I'm booking"
-    doc = nlp(input)
-    input2 = "He rushed to the train to book the ticket.  But he didn't have any food.  He ran to the shop to buy food.  It was very expensive.  How can he afford it?  He must go to the bank"
-    doc2 = nlp(input2)
+    #input = "I wish that I am somewhere else with Mrs. Smith.  I want to fly to new york or tokyo.  Anywhere but here,say.  I'll book a ticket tomorrow.  Do I have any money?  I will need money when I'm booking"
+    #doc = nlp(input)
+    #input2 = "He rushed to the train to book the ticket.  But he didn't have any food.  He ran to the shop to buy food.  It was very expensive.  How can he afford it?  He must go to the bank"
+    #doc2 = nlp(input2)
 
+    #doc1 = "Sugar is bad to consume. My sister likes to have sugar, but not my father."
+    #doc2 = "My father spends a lot of time driving my sister around to dance practice."
+    #doc3 = "Doctors suggest that driving may cause increased stress and blood pressure."
+    #doc4 = "Sometimes I feel pressure to perform well at school, but my father never seems to drive my sister to do better."
+    #doc5 = "Health experts say that Sugar is not good for your lifestyle."
+
+    # compile documents into one structure
+    #doc_list = [doc1, doc2, doc3, doc4, doc5]
+    data = pd.read_csv("databases/mojo_editorial.csv")
+    data = data['ENGLISH (Linkedin / FB)  - BITIC CHECK'].dropna()
+    print(data.head(10))
+    doc_list = data.values.tolist()
+    # print(doc_list)
+    doc_list_final = []
     stop_list = ["Mrs.", "Ms.", "say", "WASHINGTON", "'s", "Mr.", ]
     # Updates spaCy's default stop words (mapped to STOP_WORDS) list with my additional words.
     nlp.Defaults.stop_words.update(stop_list)
@@ -93,33 +111,36 @@ def topic_model2():
     # PROCESSING THE TEXT
     # ALL words in the language Vocab have attributes that are assigned (e.g. is_stop)
     # NLP has already lemmatized; now we need to create a new doc that uses root tokens
-    clean_text = lemmatize(doc)
-    #print('lemmatized doc',clean_text)
-    # remove the stop words and export text
-    clean_text = remove_stopwords(clean_text)
-
+    # use nlp.pipe for efficient data processing (we havent disabled pipeline components, eg NER)
+    for doc in nlp.pipe(doc_list):
+        doc = lemmatize(doc)
+        doc = remove_stopwords(doc)
+        #print("TEXT IS READY FOR TOPIC MODELLING:", doc)
+        doc_list_final.append(doc)
+    #print('Doc List:', doc_list_final)
     # repeat for dox2
-    clean_text2 = lemmatize(doc2)
-    clean_text2 = remove_stopwords(clean_text2)
-    print("TEXT IS READY FOR TOPIC MODELLING:", clean_text)
-    print("TEXT IS READY FOR TOPIC MODELLING:", clean_text2)
+    #clean_text2 = lemmatize(doc2)
+    #clean_text2 = remove_stopwords(clean_text2)
+    #print("TEXT IS READY FOR TOPIC MODELLING:", clean_text)
+    #print("TEXT IS READY FOR TOPIC MODELLING:", clean_text2)
+
     # PREPARE THE DOCUMENT LIST FOR TOPIC MODELLING (GENSIM only accepts TEXT so be careful that you are inputting Tokens.text)
-    doc_list = []
-    doc_list.append(clean_text)
-    doc_list.append(clean_text2)
+    #doc_list = []
+    # doc_list.append(clean_text)
+    # doc_list.append(clean_text2)
 
     # GENSHIM SETUP
     # Creates, which is a mapping of word IDs to words.
    # dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
-    words = corpora.Dictionary(doc_list)
+    words = corpora.Dictionary(doc_list_final)
 
     # Turns each document into a bag of words.
-    corpus = [words.doc2bow(doc) for doc in doc_list]
+    corpus = [words.doc2bow(doc) for doc in doc_list_final]
 
     # generate the an unsuperivsed LDA statistic analysis; using 10 topics
     lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                                 id2word=words,
-                                                num_topics=4,
+                                                num_topics=20,
                                                 random_state=2,
                                                 update_every=1,
                                                 passes=10,
@@ -128,14 +149,17 @@ def topic_model2():
     # print the model result
     # the model attributes such as topics can be manipulated
     pprint.pprint(lda_model.print_topics(num_words=4))
+    # print the topics with the highest coherence score
     top_topics = lda_model.top_topics(corpus)  # , num_words=20)
     pprint.pprint(lda_model.top_topics(corpus))
 
     # EXPLORE
 
     # visualise the topic output - show the most likely topic fro each document
-    for doc in doc_list:
-        print(lda_model.get_document_topics(words.doc2bow(doc)))
+    # for doc in doc_list_final:
+    #    print('doc=', doc)
+    #    print('most likely topic =',
+    #          lda_model.get_document_topics(words.doc2bow(doc)))
     # visualise
     #lda_display = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary, sort_topics=False)
     # pyLDAvis.display(lda_display)
@@ -148,12 +172,14 @@ def topic_model2():
     # visualise the topic output - show the most likely topic fro each document
 
     # TEST with unseen data
-    test = ["book", "bread", "eat", "train", "ticket"]
+    test = ["mojo", "design", "ai", "japan", "startup"]
     print('test model with unseen data to see its likely topics ', test)
     print(lda_model.get_document_topics(words.doc2bow(test)))
     print('Visualise')
    # pyLDAvis.enable_notebook()
     #vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+
+    topic_wordcloud(lda_model)
 
 
 def lemmatize(doc):
@@ -181,17 +207,37 @@ def remove_stopwords(doc):
     # The add_pipe function appends our functions to the DEFAULT pipeline.
 
 
-def wordcloud(papers):
-    # Import the wordcloud library
-    # Join the different processed titles together.
-    long_string = ','.join(list(papers['paper_text_processed'].values))
-    # Create a WordCloud object
-    wordcloud = WordCloud(background_color="white", max_words=5000,
-                          contour_width=3, contour_color='steelblue')
-    # Generate a word cloud
-    wordcloud.generate(long_string)
-    # Visualize the word cloud
-    wordcloud.to_image()
+def topic_wordcloud(lda_model):
+    # more colors: 'mcolors.XKCD_COLORS'
+    cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+
+    cloud = WordCloud(stopwords=None,
+                      background_color='white',
+                      width=2500,
+                      height=1800,
+                      max_words=10,
+                      colormap='tab10',
+                      color_func=lambda *args, **kwargs: cols[i],
+                      prefer_horizontal=1.0)
+
+    topics = lda_model.show_topics(
+        num_topics=10, num_words=10, formatted=False)
+
+    fig, axes = plt.subplots(2, 2, figsize=(5, 5), sharex=True, sharey=True)
+
+    for i, ax in enumerate(axes.flatten()):
+        fig.add_subplot(ax)
+        topic_words = dict(topics[i][1])
+        cloud.generate_from_frequencies(topic_words, max_font_size=300)
+        plt.gca().imshow(cloud)
+        plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+        plt.gca().axis('off')
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.axis('off')
+    plt.margins(x=0, y=0)
+    plt.tight_layout()
+    plt.show()
 
 
 # open the txt file for analysis
